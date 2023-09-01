b2b9fadfa03f27150fdeec69b9d4eda371e84538
Ardaeren00 <ardaeren000@outlook.com>
2023-09-01 14:33:48 +0300

    customer Info Detail Head component complated

--- Screens/CustomerInfoDetailPage.js
+++ Screens/CustomerInfoDetailPage.js
@@ -1,11 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, Text, } from 'react-native'
 import React from 'react'
 import CustomerInfoDetailHeadArea from "../component/CustomerInfo/CustomerInfoDetailPageComp/CustomerInfoDetailHeadArea"
 
 const CustomerInfoDetailPage = () => {
     return (
-        <CustomerInfoDetailHeadArea customerInfoDetailCustomerID="1234" customerInfoDetailCustomerNAME="RIDO" />
+        <><CustomerInfoDetailHeadArea
+            customerInfoDetailCustomerID="4312RU3M18"
+            customerInfoDetailCustomerNAME="MMM Migros Ümraniye"
+            customerInfoDetailCustomerLogin="1 Eylül 14:23" />
+            
+            <CustomerInfoDetailHeadArea
+                customerInfoDetailCustomerID="1234"
+                customerInfoDetailCustomerNAME="RIDO"
+                customerInfoDetailCustomerLogin="SALI" /></>
     )
 }
+
+
 
 export default CustomerInfoDetailPage;
--- component/CustomerInfo/CustomerInfoDetailPageComp/CustomerInfoDetailHeadArea.js
+++ component/CustomerInfo/CustomerInfoDetailPageComp/CustomerInfoDetailHeadArea.js
@@ -1,16 +1,128 @@
+import { FontAwesome } from "@expo/vector-icons";
 import React from "react";
-import { StyleSheet, View, SafeAreaView, ScrollView, Button ,Text} from 'react-native';
+import { StyleSheet, View, SafeAreaView, ScrollView, Button, Text, TouchableOpacity } from 'react-native';
 
 
-const CustomerInfoDetail = ({customerInfoDetailCustomerID
-    ,customerInfoDetailCustomerNAME })=>{
-        return(
-            <View>
-                <Text>{customerInfoDetailCustomerID}</Text>
-                <Text>{customerInfoDetailCustomerNAME}</Text>
 
+
+const CustomerInfoDetailHeadArea = ({ customerInfoDetailCustomerID
+    , customerInfoDetailCustomerNAME, customerInfoDetailCustomerLogin }) => {
+    return (
+        <SafeAreaView>
+
+
+            <View style={styles.CustomerInfoDetailHeadAreaContainer}>
+                <View>
+                    <View >
+                        <Text style={styles.CustomerInfoDetailHeadAreaIDText}>{customerInfoDetailCustomerID}</Text>
+                        <Text style={styles.CustomerInfoDetailHeadAreaIDName}>{customerInfoDetailCustomerNAME}</Text>
+
+                    </View>
+
+                    <View>
+                        <View>
+
+                            <TouchableOpacity style={styles.CustomerInfoDetailHeadAreaContainerLeftSideButton} >
+                                <FontAwesome name="map-marker" size={16} color="white" style={{ padding: 3 }} />
+
+                                <Text>Ziyaret Girişi</Text>
+                            </TouchableOpacity>
+                            <Text style={styles.CustomerInfoDetailHeadAreaContainerLeftSidePartTwoLoginText}>{customerInfoDetailCustomerLogin}</Text>
+
+
+                        </View>
+                    </View>
+
+
+
+
+
+                </View>
+                <View style={styles.CustomerInfoDetailHeadAreaContainerRightSide}>
+
+                    <TouchableOpacity style={styles.CustomerInfoDetailHeadAreaContainerRightSideButton} >
+                        <FontAwesome name="location-arrow" size={16} color="white" style={{ padding: 3 }} />
+
+                        <Text style={styles.CustomerInfoDetailHeadAreaContainerRightSideButtonText}>Tarif</Text>
+                    </TouchableOpacity>
+
+                </View>
             </View>
-            )
+
+
+
+
+        </SafeAreaView>
+
+    )
+}
+
+const styles = StyleSheet.create({
+    SafeAreaView: {
+        flex: 1
+    },
+    CustomerInfoDetailHeadAreaContainer: {
+        flexDirection: "row",
+        backgroundColor: "#f0f0f0",
+        marginVertical: 30,
+        marginHorizontal: 10,
+        borderRadius: 5,
+        minHeight: 50,
+        justifyContent: "space-between",
+        alignItems: "center", 
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+
+    CustomerInfoDetailHeadAreaContainerRightSide: {
+        backgroundColor: "#134A8D",
+        borderRadius: 3,
+        margin: 10,
+
+    },
+    CustomerInfoDetailHeadAreaContainerRightSideButton: {
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    CustomerInfoDetailHeadAreaContainerLeftSideButton: {
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#009FDC",
+        borderRadius: 5,
+        marginHorizontal: 10,
+        padding: 5,
+    },
+    CustomerInfoDetailHeadAreaContainerRightSideButtonText: {
+        fontSize: 12,
+        color: "white",
+        padding: 10,
+
+    },
+
+    CustomerInfoDetailHeadAreaIDText: {
+        padding: 5,
+        color: "black"
+
+
+    },
+    CustomerInfoDetailHeadAreaIDName: {
+        color: "black",
+        padding: 5,
+    },
+    CustomerInfoDetailHeadAreaContainerLeftSidePartTwoLoginText: {
+        padding: 5,
+        marginHorizontal: 10,
+
     }
 
-    export default CustomerInfoDetail;
+})
+
+export default CustomerInfoDetailHeadArea;
