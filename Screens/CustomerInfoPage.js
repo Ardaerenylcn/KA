807d52c936a1000dae471a83d69052cb3fe9d54d
Ardaeren00 <ardaeren000@outlook.com>
2023-09-07 15:28:02 +0300

    CustomerInfo page padding fix

--- Screens/CustomerInfoPage.js
+++ Screens/CustomerInfoPage.js
@@ -1,134 +1,140 @@
 // CustomerInfoPage.js
 
-import React from "react";
+import React from 'react';
 import {
-  StyleSheet,
-  View,
-  SafeAreaView,
-  ScrollView,
-  Button,
-  TouchableOpacity,
-  Text,
-} from "react-native";
-import { StatusBar } from "expo-status-bar";
-import CustomerInfoRow from "../component/CustomerInfo/CustomerInfoRow";
-import CustomerInfoTiltle from "../component/CustomerInfo/CustomerInfoTiltle";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NavigationContainer, useNavigation } from "@react-navigation/native";
-import CustomerInfoDetailPage from "./CustomerInfoDetailPage";
-import { FontAwesome } from "@expo/vector-icons";
-import colors from "../constants/colors";
-import fonts from "../constants/fonts";
-import distances from "../constants/distances";
-import CustomerInfoHead from "../component/CustomerInfo/customerInfoHead";
+	StyleSheet,
+	View,
+	SafeAreaView,
+	ScrollView,
+	Button,
+	TouchableOpacity,
+	Text,
+} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import CustomerInfoRow from '../component/CustomerInfo/CustomerInfoRow';
+import CustomerInfoTiltle from '../component/CustomerInfo/CustomerInfoTiltle';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import CustomerInfoDetailPage from './CustomerInfoDetailPage';
+import { FontAwesome } from '@expo/vector-icons';
+import colors from '../constants/colors';
+import fonts from '../constants/fonts';
+import distances from '../constants/distances';
+import CustomerInfoHead from '../component/CustomerInfo/customerInfoHead';
 
 function CustomerInfoPage({ navigation }) {
-  return (
-    <SafeAreaView style={styles.SafeAreaView}>
-      <CustomerInfoHead
-        customerInfoStoreCode="280025"
-        customerInfoStoreName="GALLERİA MMM MİGROS"
-        customerInfoStoreFullName="MİGROS TİCARET A.Ş"
-        navigation={navigation}
-        customerInfoStoreIsFalse=""
-      />
+	return (
+		<SafeAreaView style={styles.SafeAreaView}>
+			<CustomerInfoHead
+				customerInfoStoreCode="280025"
+				customerInfoStoreName="GALLERİA MMM MİGROS"
+				customerInfoStoreFullName="MİGROS TİCARET A.Ş"
+				navigation={navigation}
+				customerInfoStoreIsFalse=""
+			/>
 
-      <ScrollView>
-        <View style={styles.header}>
-          <CustomerInfoTiltle generalTitle="Genel Bilgiler" />
-          <CustomerInfoRow title="Mağza kodu" desc="10" />
-          <CustomerInfoRow title="İşyeri no." desc="10" />
-          <CustomerInfoRow title="Transit / Depo" desc="Transit" />
-          <CustomerInfoRow
-            title="Teslimat Adresi "
-            desc="Street:  Alsancak        
-                                           City:Konak State/province/area: Izmir 
-                                            Phone number  232 4637682
-                                            Country calling code   
-                                            +90 1234567
-                                            Country  Turkey"
-          />
-          <CustomerInfoRow title="Teslimat ili" desc="İzmir" />
-          <CustomerInfoRow title="Teslimat ilçesi" desc="Alsancak" />
-          <CustomerInfoRow title="Posta Kodu" desc="35000" />
-          <CustomerInfoRow title="Ruhsat adresi" desc="Alsancak 009253" />
-          <CustomerInfoRow title="Ruhsat No." desc="0000" />
-          <CustomerInfoRow title="Telefon No." desc="0000" />
+			<ScrollView>
+				<View style={styles.header}>
+					<CustomerInfoTiltle generalTitle="Genel Bilgiler" />
+					<CustomerInfoRow title="Mağza kodu" desc="344024800" />
+					<CustomerInfoRow title="İşyeri no." desc="304" />
+					<CustomerInfoRow title="Transit / Depo" desc="Transit" />
+					<CustomerInfoRow
+						title="Teslimat Adresi "
+						desc="SAHİL YOLU GALLERİA AVM ATAKÖY"
+					/>
+					<CustomerInfoRow title="Teslimat ili" desc="İstanbul" />
+					<CustomerInfoRow title="Teslimat ilçesi" desc="BAKIRKÖY" />
+					<CustomerInfoRow title="Posta Kodu" desc="34142" />
+					<CustomerInfoRow title="Ruhsat adresi" desc="BAKIRKÖY" />
+					<CustomerInfoRow title="Ruhsat No." desc="34042096PI" />
+					<CustomerInfoRow title="Telefon No." desc="2126614491" />
 
-          <CustomerInfoTiltle generalTitle="Müşteri Özelliği" />
-          <CustomerInfoRow title="Müşteri Grubu" desc="Migros" />
-          <CustomerInfoRow title="Müşteri Alt Grubu" desc="MMM Migros" />
-          <CustomerInfoRow title="Satış ofisi" desc="İstanbul" />
-          <CustomerInfoRow
-            title="KA Merkez Müdürü"
-            desc="Ulusal Zincir Mağzalar Özgür Zümrüt"
-          />
-          <CustomerInfoRow
-            title="KA Saha Müdürü"
-            desc=" İst-Haliç Ulusal Zincir Mağzalar ONUR KAYADELEN"
-          />
-          <CustomerInfoRow
-            title="KA Yönteicisi"
-            desc=" İst-Haliç Ulusal Zincir Mağzalar SABRİ EKRAN AKAN"
-          />
+					<CustomerInfoTiltle generalTitle="Müşteri Özelliği" />
+					<CustomerInfoRow title="Müşteri Grubu" desc="Migros" />
+					<CustomerInfoRow title="Müşteri Alt Grubu" desc="MMM Migros" />
+					<CustomerInfoRow title="Satış ofisi" desc="İstanbul Haliç Md." />
+					<CustomerInfoRow
+						title="KA Merkez Müdürü"
+						desc="Ulusal Zincir Mağzalar Satış Müdürü"
+						descriptionName="ÖZGÜR ZÜMRÜT"
+					/>
+					<CustomerInfoRow
+						title="KA Saha Müdürü"
+						desc=" İst-Haliç Ulusal Zincir Mağzalar"
+						descriptionName="ONUR KAYADELEN"
+					/>
+					<CustomerInfoRow
+						title="KA Yönteicisi"
+						desc=" İst-Haliç Ulusal Zincir Mağzalar S"
+						descriptionName="SABRİ ERMAN AKTAN"
+					/>
 
-          <CustomerInfoRow
-            title="Müşteri Özel Bölge"
-            desc=" MARMARA BÖLGESİ"
-          />
+					<CustomerInfoRow title="Müşteri Özel Bölge" desc=" MARMARA BÖLGESİ" />
+					<CustomerInfoRow title="Alan Direktörlüğü" desc="Marmara-Avrupa" />
+					<CustomerInfoRow
+						title="Nielsen Araştırma Bölgesi"
+						desc="İstanbul Avrupa"
+					/>
 
-          <CustomerInfoTiltle generalTitle="Organizasyon" />
-          <CustomerInfoRow title="Veri" desc="VERİ" />
-          <CustomerInfoRow title="Veri" desc="VERİ" />
+					<CustomerInfoTiltle generalTitle="Organizasyon" />
+					<CustomerInfoRow title="Sevk Bilgisi" desc="B&D Dağıtımlı" />
+					<CustomerInfoRow title="Veri" desc="ESENLER-DIST.-İST-HALİÇ" />
 
-          <CustomerInfoRow title="Veri" desc="VERİ" />
-          <CustomerInfoRow title="Veri" desc="VERİ" />
+					<CustomerInfoRow
+						title="Taşıcı Bayi Deposu"
+						desc="ESENLER-DIST.-İST-HALİÇ"
+					/>
+					<CustomerInfoRow title="KA Hiyerarşi 1 " desc="Retail" />
 
-          <CustomerInfoRow title="Veri" desc="VERİ" />
-          <CustomerInfoRow title="Veri" desc="VERİ" />
+					<CustomerInfoRow title="KA Hiyerarşi 2" desc="Retail Ulusal" />
+					<CustomerInfoRow title="Müşteri Durumu " desc="Aktif" />
+					<CustomerInfoRow title="Fatura Kesilmeme Nedeni" desc="" />
 
-          <CustomerInfoTiltle generalTitle="Ek veriler" />
-          <CustomerInfoRow title="Veri" desc="VERİ" />
-          <CustomerInfoRow title="Veri" desc="VERİ" />
-          <CustomerInfoRow title="Veri" desc="VERİ" />
-          <CustomerInfoRow title="Veri" desc="VERİ" />
-          <CustomerInfoRow title="Veri" desc="VERİ" />
-
-        </View>
-        <View style={styles.CustomerInfoPageScrollView}></View>
-      </ScrollView>
-      <StatusBar style="auto" />
-    </SafeAreaView>
-  );
+					<CustomerInfoTiltle generalTitle="Ek veriler" />
+					<CustomerInfoRow title="Konum Grubu" desc="Alışveriş merkezi" />
+					<CustomerInfoRow
+						title="Dağıtım Günleri"
+						desc="Pazartesi, Salı, Çarşamba, Perşembe, Cuma, Cumartesi, Pazar,"
+					/>
+					<CustomerInfoRow title="Toplam M²" desc=">2500M²" />
+					<CustomerInfoRow title="Reyon M²" desc=" " />
+					<CustomerInfoRow title="Çalışma Dönemi" desc="Standart Çalışma" />
+				</View>
+				<View style={styles.CustomerInfoPageScrollView}></View>
+			</ScrollView>
+			<StatusBar style="auto" />
+		</SafeAreaView>
+	);
 }
 
 const Stack = createNativeStackNavigator();
 
 function CustomerInfoPageNavigate() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Müşteri Bilgileri"
-        component={CustomerInfoPage}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="Müşteri İşlemleri"
-        options={{ headerBackTitle: "Geri" }}
-        component={CustomerInfoDetailPage}
-      />
-    </Stack.Navigator>
-  );
+	return (
+		<Stack.Navigator>
+			<Stack.Screen
+				name="Müşteri Bilgileri"
+				component={CustomerInfoPage}
+				options={{ headerShown: false }}
+			/>
+			<Stack.Screen
+				name="Müşteri İşlemleri"
+				options={{ headerBackTitle: 'Geri' }}
+				component={CustomerInfoDetailPage}
+			/>
+		</Stack.Navigator>
+	);
 }
 
 export default CustomerInfoPageNavigate;
 
 const styles = StyleSheet.create({
-  SafeAreaView: {
-    flex: 1,
-    backgroundColor: colors.white,
-  },
-  CustomerInfoPageScrollView: {
-    borderBottomWidth: 0.7,
-  },
+	SafeAreaView: {
+		flex: 1,
+		backgroundColor: colors.white,
+	},
+	CustomerInfoPageScrollView: {
+		borderBottomWidth: 0.7,
+	},
 });
--- component/CustomerInfo/CustomerInfoRow.js
+++ component/CustomerInfo/CustomerInfoRow.js
@@ -1,41 +1,52 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import colors from "../../constants/colors";
-import distances from "../../constants/distances";
-import fonts from "../../constants/fonts";
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import colors from '../../constants/colors';
+import distances from '../../constants/distances';
+import fonts from '../../constants/fonts';
 
-const CustomerInfoRow = ({ title, desc }) => {
-  return (
-    <View style={styles.CustomerInfoRow}>
-      <Text style={styles.CustomerInfoRowtitle}>{title}</Text>
-      <View style={styles.decContainer}>
-        <Text style={styles.dec}>{desc}</Text>
-      </View>
-    </View>
-  );
+const CustomerInfoRow = ({ title, desc, descriptionName }) => {
+	return (
+		<View style={styles.CustomerInfoRow}>
+			<View style={styles.titleContainer}>
+				<Text style={styles.CustomerInfoRowtitle}>{title}</Text>
+			</View>
+
+			<View style={styles.decContainer}>
+				<Text style={styles.dec}>
+					{desc.split('\n').map((line, index) => (
+						<Text key={index}>
+							{line}
+							{index < desc.split('\n').length - 1 && '\n'}
+						</Text>
+					))}
+				</Text>
+				<Text style={styles.dec}>{descriptionName}</Text>
+			</View>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  CustomerInfoRow: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    paddingHorizontal: distances.defaultDistance,
-    paddingVertical: distances.halfDistance,
-    borderBottomWidth: 0.5,
-    borderBottomColor: colors.borderColor,
-  },
-  CustomerInfoRowtitle: {
-    fontSize: fonts.normal,
-    flex: 1,
-    
-  },
-  decContainer: {
-    flex: 1,
-    alignItems: "flex-end",
-  },
-  dec: {
-    color: colors.grey3,
-  },
+	CustomerInfoRow: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		paddingHorizontal: distances.defaultDistance,
+		paddingVertical: distances.halfDistance,
+		borderBottomWidth: 0.5,
+		borderBottomColor: colors.borderColor,
+    marginTop:distances.quarterDistance 
+	},
+	CustomerInfoRowtitle: {
+		fontSize: fonts.normal,
+		flex: 1,
+	},
+	decContainer: {
+		flex: 1,
+		alignItems: 'flex-end',
+	},
+	dec: {
+		color: colors.grey3,
+	},
 });
 
 export default CustomerInfoRow;
