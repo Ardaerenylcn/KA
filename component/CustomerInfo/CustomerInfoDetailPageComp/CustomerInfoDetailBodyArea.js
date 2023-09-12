9d2978b85696c0a129df5739810cd29881dc39bd
Ardaeren00 <ardaeren000@outlook.com>
2023-09-12 16:53:01 +0300

    Refactor header margin to prevent overlap with breadcrumb text Previously, the header had overlapping issues with the breadcrumb text. This commit refactors the header margin to resolve this problem and improve the layout

--- component/CustomerInfo/CustomerInfoDetailPageComp/CustomerInfoDetailBodyArea.js
+++ component/CustomerInfo/CustomerInfoDetailPageComp/CustomerInfoDetailBodyArea.js
@@ -1,84 +1,135 @@
-/* eslint-disable no-empty-pattern */
-/* eslint-disable no-shadow */
-/* eslint-disable react/jsx-indent */
-/* eslint-disable no-use-before-define */
 import React, { useState } from 'react';
 import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  ScrollView,
+	View,
+	Text,
+	StyleSheet,
+	ScrollView,
+	TouchableOpacity,
+	Dimensions,
 } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome'; // Import Font Awesome Icon
+import Icon from 'react-native-vector-icons/FontAwesome';
 import colors from '../../../constants/colors';
 import distances from '../../../constants/distances';
+import fonts from '../../../constants/fonts';
 
-const CustoemrInfoDetailBodyArea = ({}) => {
-  const [selectedItem, setSelectedItem] = useState('');
+const App = () => {
+	const data = [
+		{ text: 'Ziyaret Listesi', icon: 'angle-right' },
+		{ text: 'Soğutucu Envanteri', icon: 'angle-right' },
+		{ text: 'Soğutucu Ekipman Sayımı', icon: 'angle-right' },
+		{ text: 'Soğutucu İşlemleri', icon: 'angle-right' },
+	];
 
-  const data = [
-    'Ziyaret Listesi',
-    'Soğutucu Envanteri',
-    'Soğutucu Ekipman Sayımı',
-    'Soğutucu işlemleri',
-  ];
-  return (
-    <View style={styles.CustomerInfoDetailBodyAreaContainer}>
-      <View style={styles.CustomerInfoDetailBodyAreaContainerMini}>
-        {data.map((item, index) => (
-          <TouchableOpacity
-            key={index}
-            style={[
-              styles.listItem,
-              index === data.length - 1 && styles.lastListItem,
-            ]}
-            onPress={() => {
+	const additionalTexts = {
+		'Soğutucu İşlemleri': ['Arıza Bildirimi', 'Kurulum', 'Sökme'],
+	};
 
-            }}
-          >
-            <Text style={styles.listItemText}>{item}</Text>
-            <Icon name="angle-right" size={20} color={colors.grey3} />
-          </TouchableOpacity>
-        ))}
-      </View>
-    </View>
-  );
+	const [isExpanded, setIsExpanded] = useState(false);
+
+	const handleItemPress = item => {
+		if (item.text === 'Soğutucu İşlemleri') {
+			setIsExpanded(!isExpanded);
+		}
+	};
+
+	const viewWidth = Dimensions.get('window').width;
+
+	return (
+		<View style={styles.container}>
+			<ScrollView style={styles.listContainer}>
+				{data.map((item, index) => (
+					<TouchableOpacity
+						key={index}
+						style={[
+							styles.listItem,
+							item.text === 'Soğutucu İşlemleri' ? styles.noBorder : null,
+						]}
+						onPress={() => handleItemPress(item)}
+					>
+						<Text style={styles.text}>{item.text}</Text>
+						<Icon
+							name={item.icon}
+							size={20}
+							color="black"
+							style={styles.icon}
+						/>
+					</TouchableOpacity>
+				))}
+			</ScrollView>
+			{isExpanded && (
+				<View
+					style={[
+						styles.additionalTextContainer,
+						styles.listContainer,
+						{ width: viewWidth },
+					]}
+				>
+					{additionalTexts['Soğutucu İşlemleri'].map((text, index) => (
+						<TouchableOpacity key={index} style={styles.additionalItem}>
+							<Text style={styles.additionalItemText}>{text}</Text>
+						</TouchableOpacity>
+					))}
+				</View>
+			)}
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  CustomerInfoDetailBodyAreaContainer: {
-    paddingVertical: distances.defaultDistance,
-    backgroundColor: colors.grey4,
-    margin: distances.defaultDistance,
-    borderRadius: distances.quarterDistance,
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.3,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  CustomerInfoDetailBodyAreaContainerMini: {
-    padding: distances.defaultDistance,
-    marginHorizontal: distances.defaultDistance,
-  },
-  listItem: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    borderBottomWidth: 0.3,
-    borderBottomColor: colors.grey3,
-    paddingVertical: 10,
-  },
-  lastListItem: {
-    borderBottomWidth: 0, // Remove border from the last item
-  },
-  listItemText: {
-    fontSize: 16,
-    marginRight: 10,
-  },
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: 'white',
+		padding: distances.defaultDistance,
+		margin: distances.defaultDistance,
+		borderRadius: 10,
+		shadowOffset: {
+			width: 0,
+			height: 2,
+		},
+		shadowOpacity: 0.3,
+		shadowRadius: 4,
+		elevation: 5,
+	},
+	listContainer: {
+		width: '100%',
+	},
+	listItem: {
+		borderBottomWidth: 1,
+		backgroundColor: colors.backgroundColor,
+		borderColor: colors.borderColor,
+		flexDirection: 'row',
+		alignItems: 'center',
+		padding: distances.defaultDistance / 1.5,
+		borderRadius: 5,
+	},
+	text: {
+		flex: 1,
+		fontSize: fonts.big,
+	},
+	icon: {
+		marginLeft: distances.defaultDistance,
+		color: 'grey',
+	},
+	additionalTextContainer: {
+		backgroundColor: colors.backgroundColor,
+		paddingHorizontal: 20,
+	},
+	additionalItem: {
+		backgroundColor: colors.backgroundColor,
+		padding: distances.defaultDistance / 1.5,
+		marginHorizontal: distances.defaultDistance,
+		borderBottomWidth: 1,
+		borderColor: colors.borderColor,
+	},
+	additionalItemText: {
+		color: 'black',
+		fontSize: fonts.big,
+	},
+	noBorder: {
+		borderBottomWidth: 0,
+	},
 });
 
-export default CustoemrInfoDetailBodyArea;
+export default App;
