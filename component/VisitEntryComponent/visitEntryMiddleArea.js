b068e5a2987b7c9e665ecea2cbb5860269ea5f5c
Ardaeren00 <ardaeren000@outlook.com>
2023-09-14 09:50:18 +0300

    feat: Update VisitEntryMiddleArea component styling
    - Adjusted the styling of the 'VisitEntryMiddleArea' component for better visual consistency.
    - Tweaked margins, padding, and text input appearance.
    - Ensured proper spacing and alignment.

--- component/VisitEntryComponent/visitEntryMiddleArea.js
+++ component/VisitEntryComponent/visitEntryMiddleArea.js
@@ -1,68 +1,74 @@
 import React, { useState } from 'react';
-import { Text, View, StyleSheet, Switch, TextInput,ScrollView } from 'react-native';
+import {
+	Text,
+	View,
+	StyleSheet,
+	Switch,
+	TextInput,
+	ScrollView,
+} from 'react-native';
 import colors from '../../constants/colors';
 import fonts from '../../constants/fonts';
 import distances from '../../constants/distances';
 
 const VisitEntryMiddleArea = () => {
-    const [value, onChangeText] = React.useState('Useless Multiline Placeholder');
+	const [number, onChangeNumber] = React.useState('');
 
-  return (
-
-      <View style={styles.visitEntryContainer}>
-        <View style={styles.visitEntryTitleContainer}>
-          <Text style={styles.visitEntryTitle}>AÇIKLAMA GİRİNİZ</Text>
-        </View>
-        <View style={styles.visitEntryDesc}>
-        <TextInput
-        editable={true}
-        multiline
-        numberOfLines={4}
-        maxLength={40}
-        onChangeText={text => onChangeText(text)}
-        value={value}
-        style={{padding: 10}}
-      />
-
-        </View>
-      </View>
-
-  );
+	return (
+		<View style={styles.visitEntryContainer}>
+			<View style={styles.visitEntryTitleContainer}>
+				<Text style={styles.visitEntryTitle}>AÇIKLAMA GİRİNİZ</Text>
+			</View>
+			<View style={styles.visitEntryDesc}>
+				<ScrollView>
+					<TextInput
+						style={styles.visitEntryDescTextInput}
+						onChangeText={onChangeNumber}
+						value={number}
+						placeholder="Nokta ile ilgili açıklamalarınızı bu alana giriniz"
+						keyboardType="numeric"
+						fontSize={16}
+						multiline={true}
+						editable={true}
+					/>
+				</ScrollView>
+			</View>
+		</View>
+	);
 };
 
-
 const styles = StyleSheet.create({
-  visitEntryContainer: {
-    paddingVertical: distances.defaultDistance,
-    backgroundColor: colors.white,
-    margin: distances.defaultDistance,
-    borderRadius: distances.quarterDistance,
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.3,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  visitEntryTitle: {
-    marginLeft: distances.halfDistance,
-    color: colors.grey,
-    fontWeight: '500',
-    marginBottom: distances.halfDistance,
-  },
-  visitEntryTitleContainer: {
-    borderBottomWidth: 1,
-    borderBottomColor: colors.borderColor,
-    marginHorizontal: distances.defaultDistance,
-  },
-  visitEntryDescTextInput: {
-    marginVertical:distances.defaultDistance,
-    marginHorizontal: distances.defaultDistance *2,
-    padding: distances.halfDistance,
-  },
+	visitEntryContainer: {
+		paddingVertical: distances.defaultDistance,
+		backgroundColor: colors.white,
+		marginHorizontal: distances.defaultDistance,
+		borderRadius: distances.quarterDistance,
+		shadowOffset: {
+			width: 0,
+			height: 2,
+		},
+		shadowOpacity: 0.3,
+		shadowRadius: 4,
+		elevation: 5,
+	},
+	visitEntryTitle: {
+		marginLeft: distances.halfDistance,
+		color: colors.grey,
+		fontWeight: '500',
+		marginBottom: distances.halfDistance,
+	},
+	visitEntryTitleContainer: {
+		borderBottomWidth: 1,
+		borderBottomColor: colors.borderColor,
+		marginHorizontal: distances.defaultDistance,
+	},
+	visitEntryDescTextInput: {
+		marginVertical: distances.defaultDistance,
+		marginHorizontal: distances.defaultDistance * 2,
+		padding: distances.halfDistance,
+		height: 70,
+		maxHeight: 70,
+	},
 });
 
 export default VisitEntryMiddleArea;
-
-
