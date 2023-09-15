753efdae16470db7c8c6dfe9cebb9b6ac6e2613a
Ardaeren00 <ardaeren000@outlook.com>
2023-09-15 10:34:56 +0300

    feat: Added Bottom Tab Navigation
    In this commit, I introduced a Bottom Tab Navigator to facilitate navigation within the app. The navigator includes three screens: DUYURULAR, Profilim, and Neredeyim. Custom styling options for headers and tab bar badges were also applied.
    
    Changes:
    - Implemented a Bottom Tab Navigator.
    - Added three screens: DUYURULAR, Profilim, and Neredeyim.
    - Customized header titles and tab bar styles.

--- App.js
+++ App.js
@@ -1,88 +1,69 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { NavigationContainer } from "@react-navigation/native";
-import Announcement from "./component/Announcement";
-import CustomerInfo from "./Screens/CustomerInfoPage";
-
-
-
-
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import Announcement from './component/Announcement';
+import CustomerInfo from './Screens/CustomerInfoPage';
 
 const Tab = createBottomTabNavigator();
 
 function Duyurular() {
-  return <Announcement  />;
+	return <Announcement />;
 }
 
 function Customer() {
-  return <CustomerInfo />;
-
-  
+	return <CustomerInfo />;
 }
 
 function ProfileScreen() {
-  return null;
+	return null;
 }
 
-
-
-
-
-
-
 function MyTabs() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen
-        name="DUYURULAR"
-        component={Duyurular}
-        options={{
-          headerTitle: 'KA MOBİL',
-          headerTintColor: 'white',
-          tabBarBadge: 4,
-          tabBarInactiveTintColor: '#9c9c9c',
-          tabBarBadgeStyle: {
-            backgroundColor: '#ff4040',
-            fontSize: 12,
-            fontWeight: '700',
-          },
+	return (
+		<Tab.Navigator>
+			<Tab.Screen
+				name="DUYURULAR"
+				component={Duyurular}
+				options={{
+					headerTitle: 'KA MOBİL',
+					headerTintColor: 'white',
+					tabBarBadge: 4,
+					tabBarInactiveTintColor: '#9c9c9c',
+					tabBarBadgeStyle: {
+						backgroundColor: '#ff4040',
+						fontSize: 12,
+						fontWeight: '700',
+					},
+				}}
+			/>
+			<Tab.Screen
+				name="Profilim"
+				component={Customer}
+				options={{
+					headerTitle: 'KA MOBİL',
+					headerTintColor: 'black',
+				}}
+			/>
 
-        }}
-      />
-      <Tab.Screen
-        name="Profilim"
-        component={Customer}
-        options={{
-          headerTitle: 'KA MOBİL',
-          headerTintColor: 'white',
-        }}/>
-
-
-      <Tab.Screen
-        name="Neredeyim"
-        component={ProfileScreen}
-        options={{
-          headerTitle: 'KA MOBiL',
-          headerTintColor: 'white',
-        }}/>
-    </Tab.Navigator>
-  );
+			<Tab.Screen
+				name="Neredeyim"
+				component={ProfileScreen}
+				options={{
+					headerTitle: 'KA MOBiL',
+					headerTintColor: 'black',
+				}}
+			/>
+		</Tab.Navigator>
+	);
 }
 
 const App = () => {
-
-  return (
-    <NavigationContainer>
-      <MyTabs />
-    </NavigationContainer>
-  );
+	return (
+		<NavigationContainer>
+			<MyTabs />
+		</NavigationContainer>
+	);
 };
-
-
-
-
-
-
 
 export default App;
