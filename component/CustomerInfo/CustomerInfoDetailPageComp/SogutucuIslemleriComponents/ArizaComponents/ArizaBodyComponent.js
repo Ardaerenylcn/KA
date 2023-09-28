e2f971d70aa88a0403ce473a1fabffccd394e8e3
Ardaeren00 <ardaeren000@outlook.com>
2023-09-28 14:23:21 +0300

    Commit type: Refactor Refactor: Remove unused imports Description: Cleaned up the code by removing unused imports that were not being utilized.

--- component/CustomerInfo/CustomerInfoDetailPageComp/SogutucuIslemleriComponents/ArizaComponents/ArizaBodyComponent.js
+++ component/CustomerInfo/CustomerInfoDetailPageComp/SogutucuIslemleriComponents/ArizaComponents/ArizaBodyComponent.js
@@ -1,12 +1,71 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
+import {
+	View,
+	Text,
+	StyleSheet,
+	TouchableOpacity,
+	TextInput,
+} from 'react-native';
+import distances from '../../../../../constants/distances';
+import colors from '../../../../../constants/colors';
+import fonts from '../../../../../constants/fonts';
 
-const ArizaBodyComponent = () => {
+const ArizaBodyComponent = ({ SerialNumber, CollerName }) => {
+	const [text, onChangeText] = useState('');
+
 	return (
-		<View>
-			<Text>ArizaBodyComponent</Text>
+		<View style={styles.ArizaBodyComponentContainer}>
+			<TouchableOpacity style={styles.ArizaBodyComponentContainerHead}>
+				<View style={styles.ArizaBodyComponentContainerHeadArea}>
+					<Text>ARIZA KAYNAĞI</Text>
+					<View style={styles.ArizaBodyComponentContainerHeadSelection}>
+						<Text style={{ color: colors.borderColor }}>İşlem Seçiniz</Text>
+						<Text>.</Text>
+					</View>
+				</View>
+			</TouchableOpacity>
+
+			<TextInput
+				style={styles.input}
+				onChangeText={onChangeText}
+				value={text}
+				placeholder={`${SerialNumber} barkod numaralı ${CollerName}  arıza açıklaması giriniz`}
+				placeholderTextColor={colors.placeholderTextColor}
+				multiline={true}
+				numberOfLines={4}
+			/>
 		</View>
 	);
 };
+
+const styles = StyleSheet.create({
+	ArizaBodyComponentContainer: {
+		flex: 1,
+		backgroundColor: colors.white,
+	},
+	ArizaBodyComponentContainerHead: {
+		flex: 1,
+		borderBottomWidth: 1,
+		borderColor: colors.borderColor,
+	},
+	ArizaBodyComponentContainerHeadSelection: {
+		flexDirection: 'row',
+	},
+	ArizaBodyComponentContainerHeadArea: {
+		marginHorizontal: distances.defaultDistance,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		marginVertical: distances.defaultDistance,
+	},
+	input: {
+		borderWidth: 1,
+		borderColor: colors.borderColor,
+		borderRadius: 4,
+		padding: distances.defaultDistance,
+		height: 120,
+		textAlignVertical: 'top',
+		fontSize: fonts.big,
+	},
+});
 
 export default ArizaBodyComponent;
