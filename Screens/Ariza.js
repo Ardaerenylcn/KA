f1008de333a512ff54a4576ec04de61dece28754
Ardaeren00 <ardaeren000@outlook.com>
2023-09-28 14:30:29 +0300

    Commit type: Refactor Refactor: Remove unused imports Description: Cleaned up the code by removing unused imports that were not being utilized.

--- Screens/Ariza.js
+++ Screens/Ariza.js
@@ -1,11 +1,24 @@
-import { View, Text, ScrollView } from 'react-native';
+import {ScrollView } from 'react-native';
 import React from 'react';
 import ArizaHeadComponent from './/../component/CustomerInfo/CustomerInfoDetailPageComp/SogutucuIslemleriComponents/ArizaComponents/ArizaHeadComponent';
-
+import ArizaBodyComponent from '../component/CustomerInfo/CustomerInfoDetailPageComp/SogutucuIslemleriComponents/ArizaComponents/ArizaBodyComponent';
 const Ariza = () => {
 	return (
 		<ScrollView>
-			<ArizaHeadComponent />
+			<ArizaHeadComponent
+				StroeCode={280025}
+				StoreFullName={'GALLERİA MMM MİGROS'}
+				Directorate={'Ulusal Zincir Mağzalar Satış Müdür'}
+				Adress={'ESENLER-DIST.-İST-HALİÇ'}
+				SerialNumber={'100176521'}
+				CollerName={'SOGUTUCU EP DOLAP 2 KAPILI 2019 LUNA'}
+				CoolerDesc={'Tanım'}
+			/>
+
+			<ArizaBodyComponent
+				SerialNumber={'100176521'}
+				CollerName={'SOGUTUCU EP DOLAP 2 KAPILI 2019 LUNA'}
+			/>
 		</ScrollView>
 	);
 };
