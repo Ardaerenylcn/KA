7e8e5b21b9cf324727570a2725f4b02edfe3f7ff
Ardaeren00 <ardaeren000@outlook.com>
2023-09-28 14:34:03 +0300

    Commit type: Refactor Refactor: Remove unused imports Description: Cleaned up the code by removing unused imports that were not being utilized.

--- Screens/VisitEntry.js
+++ Screens/VisitEntry.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import { View, Text, ScrollView } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { View, ScrollView } from 'react-native';
 import VisitEntryTopArea from '../component/VisitEntryComponent/visitEntryTopArea';
 import VisitEntryMiddleArea from '../component/VisitEntryComponent/visitEntryMiddleArea';
 import VisitEntryBottomArea from '../component/VisitEntryComponent/visitEntryBotttomArea';
 
 const VisitEntry = () => {
 	return (
-
-			<ScrollView style={{ flex: 1, marginTop: 0 }}>
-				<View>
-					<VisitEntryTopArea
-						visitEntryStoreCode="280025"
-						visitEntryStoreName="GALLERİA MMM MİGROS"
-					/>
-					<VisitEntryMiddleArea />
-					<VisitEntryBottomArea />
-				</View>
-			</ScrollView>
-
+		<ScrollView style={{ flex: 1, marginTop: 0 }}>
+			<View>
+				<VisitEntryTopArea
+					visitEntryStoreCode="280025"
+					visitEntryStoreName="GALLERİA MMM MİGROS"
+				/>
+				<VisitEntryMiddleArea />
+				<VisitEntryBottomArea />
+			</View>
+		</ScrollView>
 	);
 };
 
