80eb9cb9068f0ad0a942ad5c60178a04ec170dd0
Ardaeren00 <ardaeren000@outlook.com>
2023-09-28 14:33:01 +0300

    Commit type: Refactor Refactor: Remove unused imports Description: Cleaned up the code by removing unused imports that were not being utilized.

--- Screens/Kurulum.js
+++ Screens/Kurulum.js
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView } from 'react-native';
+import {ScrollView } from 'react-native';
 import React from 'react';
 import KurulumHeadComponent from '../component/CustomerInfo/CustomerInfoDetailPageComp/SogutucuIslemleriComponents/KurulumHeadComponent';
 import KurulumBodyComponent from '../component/CustomerInfo/CustomerInfoDetailPageComp/SogutucuIslemleriComponents/KurulumBodyComponent';
