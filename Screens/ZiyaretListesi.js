f3e4438c4fd822184019a5744ab6ea0514e85222
Ardaeren00 <ardaeren000@outlook.com>
2023-09-28 14:34:46 +0300

    Commit type: Refactor Refactor: Remove unused imports Description: Cleaned up the code by removing unused imports that were not being utilized.

--- Screens/ZiyaretListesi.js
+++ Screens/ZiyaretListesi.js
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView } from 'react-native';
+import { ScrollView } from 'react-native';
 import React from 'react';
 import ZiyaretListesiComponent from '../component/CustomerInfo/CustomerInfoDetailPageComp/ZiyaretListesiComponent';
 
