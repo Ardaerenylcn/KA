8f77796973203ec3345cd9919c2a2f7f7a3a5332
Ardaeren00 <ardaeren000@outlook.com>
2023-09-28 14:24:46 +0300

    Commit type: Refactor Refactor: Remove unused imports Description: Cleaned up the code by removing unused imports that were not being utilized.

--- component/CustomerInfo/CustomerInfoDetailPageComp/SogutucuIslemleriComponents/KurulumHeadComponent.js
+++ component/CustomerInfo/CustomerInfoDetailPageComp/SogutucuIslemleriComponents/KurulumHeadComponent.js
@@ -6,7 +6,6 @@ import {
 	StyleSheet,
 	Image,
 	TouchableOpacity,
-	Keyboard,
 } from 'react-native';
 import colors from '../../../../constants/colors';
 import distances from '../../../../constants/distances';
