162a8f0940ae9257ca2065d316c3cc61b7c2431d
Ardaeren00 <ardaeren000@outlook.com>
2023-10-01 21:09:44 +0300

    feat: Added Shadowed Style Variables
    This commit adds a style object containing shadowed style variables for use in our React Native application.

---
+++ Styles/styleVariables.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+const styleVariables = StyleSheet.create({
+	shadow: {
+		shadowOffset: {
+			width: 0,
+			height: 2,
+		},
+		shadowOpacity: 0.3,
+		shadowRadius: 4,
+		elevation: 5,
+	},
+});
+
+export default styleVariables;
