5e1af6a81f9709e5d95099a9546404534a30f65e
Ardaeren00 <ardaeren000@outlook.com>
2023-10-02 14:55:02 +0300

    git commit -m "Create SellerDistributorStockHeader SellerDistributorStockBody and sellerPage screens"

---
+++ component/SellerDistributorStockComponents/SellerDistributorStockHeader.js
@@ -0,0 +1,59 @@
+import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+import colors from '../../constants/colors';
+import distances from '../../constants/distances';
+
+const SellerDistributorStockHeader = ({ product }) => {
+	return (
+		<View style={styles.SellerDistributorStockHeaderContainer}>
+			<View style={styles.SellerDistributorStockHeaderContainerTextArea}>
+				<View style={styles.SellerDistributorStockHeaderContainerTextAreaLeft}>
+					<Text style={styles.SellerDistributorStockHeaderTextStyleLeftSide}>
+						{product}
+					</Text>
+				</View>
+				<View style={styles.SellerDistributorStockHeaderContainerTextAreaRight}>
+					<Text style={styles.SellerDistributorStockHeaderTextStyle}>
+						KA Stok
+					</Text>
+					<Text style={styles.SellerDistributorStockHeaderTextStyle}>
+						GEL.KAN.
+					</Text>
+				</View> 
+			</View>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	SellerDistributorStockHeaderContainer: {
+		flex: 1,
+		backgroundColor: colors.efesblue,
+	},
+	SellerDistributorStockHeaderContainerTextArea: {
+		flexDirection: 'row',
+	},
+	SellerDistributorStockHeaderContainerTextAreaRight: {
+		flex: 1,
+		justifyContent: 'flex-end',
+		flexDirection: 'row',
+		marginRight: distances.defaultDistance,
+	},
+	SellerDistributorStockHeaderContainerTextAreaLeft: {
+		marginLeft: distances.defaultDistance,
+	},
+
+	SellerDistributorStockHeaderTextStyle: {
+		color: colors.white,
+		fontWeight: 'bold',
+		marginLeft: distances.defaultDistance,
+		marginVertical: distances.quarterDistance / 2,
+	},
+	SellerDistributorStockHeaderTextStyleLeftSide: {
+		color: colors.white,
+		fontWeight: 'bold',
+		marginVertical: distances.quarterDistance / 2,
+	},
+});
+
+export default SellerDistributorStockHeader;
