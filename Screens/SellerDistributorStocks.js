94266407f85142ade01aba1818c9993dea3fd43b
Ardaeren00 <ardaeren000@outlook.com>
2023-10-03 17:14:16 +0300

    Added SectionList to display product data in sections.

--- Screens/SellerDistributorStocks.js
+++ Screens/SellerDistributorStocks.js
@@ -1,19 +1,22 @@
 import React, { useRef, useState } from 'react';
 import {
 	View,
-	ScrollView,
 	TouchableOpacity,
 	StyleSheet,
 	Image,
 	Animated,
 	Easing,
+	Text,
+	SafeAreaView,
+	StatusBar,
+	SectionList,
 } from 'react-native';
 import SellerDistributorAndProductStockHeader from '../component/SellerDistributorStockComponents/SellerDistirbutorHead';
 import SellerDistirbutorStockBody from '../component/SellerDistributorStockComponents/SellerDistirbutorStockBody';
 import colors from '../constants/colors';
 
 const SellerDistributorStocks = () => {
-	const scrollViewRef = useRef(null);
+	const sectionListRef = useRef(null);
 	const [isButtonVisible, setIsButtonVisible] = useState(false);
 	const pulseAnimation = useRef(new Animated.Value(1)).current;
 
@@ -25,10 +28,12 @@ const SellerDistributorStocks = () => {
 			setIsButtonVisible(false);
 		}
 	};
-
 	const handlePress = () => {
-		if (scrollViewRef.current) {
-			scrollViewRef.current.scrollTo({ x: 0, y: 0, animated: true });
+		if (sectionListRef.current) {
+			sectionListRef.current.scrollToLocation({
+				sectionIndex: 0,
+				itemIndex: 0,
+			});
 
 			Animated.sequence([
 				Animated.timing(pulseAnimation, {
@@ -47,155 +52,344 @@ const SellerDistributorStocks = () => {
 		}
 	};
 
+	const sections = [
+		{
+			title: 'Efes Pilsen',
+			data: [
+				{
+					productCode: '150003',
+					productFullName: 'EFES PİLSEN KAS 50 CL',
+					type: 'STEINIE RB',
+					stock: 216,
+					gelKan: 180,
+				},
+				{
+					productCode: '150008',
+					productFullName: 'EFES PİLSEN KL 33 CL NRB',
+					stock: 84,
+					gelKan: 123,
+				},
+			],
+		},
+		{
+			title: 'Miller Genuine Draft',
+			data: [
+				{
+					productCode: '150137',
+					productFullName: 'MGD KL 33 CL NRB',
+					stock: 46,
+					gelKan: 11,
+				},
+				{
+					productCode: '150138',
+					productFullName: 'MGD KL 33 CL NRB 4*6 MP',
+					stock: 3,
+					gelKan: 0,
+				},
+			],
+		},
+		{
+			title: "Beck's",
+			data: [
+				{
+					productCode: '150284',
+					productFullName: 'BECKS KL 33 CL NRB',
+					stock: 30,
+					gelKan: 21,
+				},
+				{
+					productCode: '150292',
+					productFullName: 'BECKS TVA 50 CL KTU',
+					stock: 60,
+					gelKan: 69,
+				},
+			],
+		},
+		{
+			title: 'Section 4',
+			data: [
+				{
+					productCode: '123456',
+					productFullName: 'Product 1',
+					type: 'Type 1',
+					stock: 100,
+					gelKan: 50,
+				},
+				{
+					productCode: '7891011',
+					productFullName: 'Product 2',
+					type: 'Type 2',
+					stock: 75,
+					gelKan: 25,
+				},
+			],
+		},
+		{
+			title: 'Section 5',
+			data: [
+				{
+					productCode: '111213',
+					productFullName: 'Product 3',
+					type: 'Type 3',
+					stock: 200,
+					gelKan: 100,
+				},
+				{
+					productCode: '141516',
+					productFullName: 'Product 4',
+					type: 'Type 4',
+					stock: 50,
+					gelKan: 10,
+				},
+			],
+		},
+		{
+			title: 'Section 6',
+			data: [
+				{
+					productCode: '171819',
+					productFullName: 'Product 5',
+					type: 'Type 5',
+					stock: 300,
+					gelKan: 150,
+				},
+				{
+					productCode: '202122',
+					productFullName: 'Product 6',
+					type: 'Type 6',
+					stock: 25,
+					gelKan: 5,
+				},
+			],
+		},
+		{
+			title: 'Section 7',
+			data: [
+				{
+					productCode: '232425',
+					productFullName: 'Product 7',
+					type: 'Type 7',
+					stock: 75,
+					gelKan: 20,
+				},
+				{
+					productCode: '262728',
+					productFullName: 'Product 8',
+					type: 'Type 8',
+					stock: 125,
+					gelKan: 60,
+				},
+			],
+		},
+		{
+			title: 'Section 8',
+			data: [
+				{
+					productCode: '293031',
+					productFullName: 'Product 9',
+					type: 'Type 9',
+					stock: 150,
+					gelKan: 70,
+				},
+				{
+					productCode: '323334',
+					productFullName: 'Product 10',
+					type: 'Type 10',
+					stock: 45,
+					gelKan: 15,
+				},
+			],
+		},
+		{
+			title: 'Section 9',
+			data: [
+				{
+					productCode: '353637',
+					productFullName: 'Product 11',
+					type: 'Type 11',
+					stock: 200,
+					gelKan: 100,
+				},
+				{
+					productCode: '383940',
+					productFullName: 'Product 12',
+					type: 'Type 12',
+					stock: 85,
+					gelKan: 30,
+				},
+				{
+					productCode: '353637',
+					productFullName: 'Product 11',
+					type: 'Type 11',
+					stock: 200,
+					gelKan: 100,
+				},
+				{
+					productCode: '383940',
+					productFullName: 'Product 12',
+					type: 'Type 12',
+					stock: 85,
+					gelKan: 30,
+				},
+			],
+		},
+		{
+			title: 'Section 10',
+			data: [
+				{
+					productCode: '414243',
+					productFullName: 'Product 13',
+					type: 'Type 13',
+					stock: 300,
+					gelKan: 150,
+				},
+				{
+					productCode: '444546',
+					productFullName: 'Product 14',
+					type: 'Type 14',
+					stock: 40,
+					gelKan: 10,
+				},
+			],
+			
+		},
+		{
+			title: 'Section 11',
+			data: [
+				{
+					productCode: '353637',
+					productFullName: 'Product 11',
+					type: 'Type 11',
+					stock: 200,
+					gelKan: 100,
+				},
+				{
+					productCode: '383940',
+					productFullName: 'Product 12',
+					type: 'Type 12',
+					stock: 85,
+					gelKan: 30,
+				},
+				{
+					productCode: '353637',
+					productFullName: 'Product 11',
+					type: 'Type 11',
+					stock: 200,
+					gelKan: 100,
+				},
+				{
+					productCode: '383940',
+					productFullName: 'Product 12',
+					type: 'Type 12',
+					stock: 85,
+					gelKan: 30,
+				},
+			],
+		},
+		{
+			title: 'Section 12',
+			data: [
+				{
+					productCode: '353637',
+					productFullName: 'Product 11',
+					type: 'Type 11',
+					stock: 200,
+					gelKan: 100,
+				},
+				{
+					productCode: '383940',
+					productFullName: 'Product 12',
+					type: 'Type 12',
+					stock: 85,
+					gelKan: 30,
+				},
+				{
+					productCode: '353637',
+					productFullName: 'Product 11',
+					type: 'Type 11',
+					stock: 200,
+					gelKan: 100,
+				},
+				{
+					productCode: '383940',
+					productFullName: 'Product 12',
+					type: 'Type 12',
+					stock: 85,
+					gelKan: 30,
+				},
+			],
+		},
+		{
+			title: 'Section 13',
+			data: [
+				{
+					productCode: '353637',
+					productFullName: 'Product 11',
+					type: 'Type 11',
+					stock: 200,
+					gelKan: 100,
+				},
+				{
+					productCode: '383940',
+					productFullName: 'Product 12',
+					type: 'Type 12',
+					stock: 85,
+					gelKan: 30,
+				},
+				{
+					productCode: '353637',
+					productFullName: 'Product 11',
+					type: 'Type 11',
+					stock: 200,
+					gelKan: 100,
+				},
+				{
+					productCode: '383940',
+					productFullName: 'Product 12',
+					type: 'Type 12',
+					stock: 85,
+					gelKan: 30,
+				},
+			],
+		},
+		
+	];
+
 	return (
-		<View>
-			{isButtonVisible && (
-				<TouchableOpacity style={styles.floatingButton} onPress={handlePress}>
-					<Animated.View
-						style={[styles.pulse, { transform: [{ scale: pulseAnimation }] }]}
-					/>
-					<Image
-						style={styles.tinyArrowLogo}
-						source={require('../assets/icons/ic-arrow-up.png')}
-					/>
-				</TouchableOpacity>
-			)}
+		<SafeAreaView style={{ flex: 1 }}>
+			<View>
+				{isButtonVisible && (
+					<TouchableOpacity style={styles.floatingButton} onPress={handlePress}>
+						<Animated.View
+							style={[styles.pulse, { transform: [{ scale: pulseAnimation }] }]}
+						/>
+						<Image
+							style={styles.tinyArrowLogo}
+							source={require('../assets/icons/ic-arrow-up.png')}
+						/>
+					</TouchableOpacity>
+				)}
 
-			<ScrollView ref={scrollViewRef} onScroll={handleScroll}>
-				<SellerDistributorAndProductStockHeader product={'Efes Pilsen'} />
-
-				<SellerDistirbutorStockBody
-					productCode={'150003'}
-					productFullName={'EFES PİLSEN KAS 50 CL'}
-					type={'STEINIE RB'}
-					stock={216}
-					gelKan={180}
+				<SectionList
+					ref={sectionListRef}
+					onScroll={handleScroll}
+					sections={sections}
+					keyExtractor={(item, index) => item.productCode + index}
+					renderItem={({ item }) => (
+						<SellerDistirbutorStockBody
+							productCode={item.productCode}
+							productFullName={item.productFullName}
+							type={item.type}
+							stock={item.stock}
+							gelKan={item.gelKan}
+						/>
+					)}
+					renderSectionHeader={({ section: { title } }) => (
+						<SellerDistributorAndProductStockHeader product={title} />
+					)}
 				/>
-				<SellerDistirbutorStockBody
-					productCode={'150008'}
-					productFullName={'EFES PİLSEN KL 33 CL NRB'}
-					stock={84}
-					gelKan={123}
-				/>
-				<SellerDistirbutorStockBody
-					productCode={'150021'}
-					productFullName={'EFES PİLSEN TVA 50 CL'}
-					type={'KTU'}
-					stock={1.657}
-					gelKan={2.215}
-				/>
-				<SellerDistirbutorStockBody
-					productCode={'151400'}
-					productFullName={'EFES PİLSEN TVA 50 CL'}
-					type={'KTU 6*4 MP'}
-					stock={647}
-					gelKan={0}
-				/>
-				<SellerDistirbutorStockBody
-					productCode={'151514'}
-					productFullName={'EFES PİLSEN TVA 33 CL'}
-					type={'SLEEK'}
-					stock={2}
-					gelKan={216}
-				/>
-				<SellerDistirbutorStockBody
-					productCode={'151830'}
-					productFullName={'EFES PİLSEN KL 30 CL'}
-					type={'STEINIE NRB*24'}
-					stock={226}
-					gelKan={0}
-				/>
-				<SellerDistirbutorStockBody
-					productCode={'151899'}
-					productFullName={'EFES PiLSEN KAS 50 CL RB'}
-					type={'YENi SiSE.'}
-					stock={404}
-					gelKan={393}
-				/>
-				<SellerDistirbutorStockBody
-					productCode={'151962'}
-					productFullName={'EFES PİLSEN TVA 50 CL KTU 4*6 MP 2021'}
-					stock={214}
-					gelKan={0}
-				/>
-
-				<SellerDistirbutorStockBody
-					productCode={'151712'}
-					productFullName={"EFES RED ALE KOLi 33 CL TY 12'Li"}
-					stock={0}
-					gelKan={18}
-				/>
-				<SellerDistirbutorStockBody
-					productCode={'151898'}
-					productFullName={"EFES RED ALE KOLi 33 CL TY 12'Li"}
-					stock={0}
-					gelKan={18}
-				/>
-				<SellerDistirbutorStockBody
-					productCode={'151898'}
-					productFullName={'EFES PiLSEN TVA 50 CL RB 6*4 MP YENiSiSE'}
-					stock={0}
-					gelKan={90}
-				/>
-				<SellerDistirbutorStockBody
-					productCode={'152101'}
-					productFullName={'EFES PiLSEN FIÇI 50 L Miller Genuine Draft'}
-					stock={0}
-					gelKan={55}
-				/>
-				<SellerDistributorAndProductStockHeader
-					product={'Miller Genuine Draft'}
-				/>
-				<SellerDistirbutorStockBody
-					productCode={'150137'}
-					productFullName={'MGD KL 33 CL NRB'}
-					stock={46}
-					gelKan={11}
-				/>
-				<SellerDistirbutorStockBody
-					productCode={'150138'}
-					productFullName={'MGD KL 33 CL NRB 4*6 MP'}
-					stock={3}
-					gelKan={0}
-				/>
-				<SellerDistirbutorStockBody
-					productCode={'150204'}
-					productFullName={'MGD TVA 50 CL KTU'}
-					stock={17}
-					gelKan={26}
-				/>
-				<SellerDistirbutorStockBody
-					productCode={'151420'}
-					productFullName={"MGD KL 50 CL NRB Beck's"}
-					stock={123}
-					gelKan={114}
-				/>
-				<SellerDistirbutorStockBody
-					productCode={'150284'}
-					productFullName={'BECKS KL 33 CL NRB'}
-					stock={30}
-					gelKan={21}
-				/>
-				<SellerDistirbutorStockBody
-					productCode={'150292'}
-					productFullName={'BECKS TVA 50 CL KTU'}
-					stock={60}
-					gelKan={69}
-				/>
-				<SellerDistirbutorStockBody
-					productCode={'151384'}
-					productFullName={'BECKS KL 50 CL NRB'}
-					stock={148}
-					gelKan={209}
-				/>
-				<SellerDistirbutorStockBody
-					productCode={'151905'}
-					productFullName={'BECKS KL 50 CL NRB 4*4'}
-					stock={166}
-					gelKan={0}
-				/>
-			</ScrollView>
-		</View>
+			</View>
+		</SafeAreaView>
 	);
 };
 
@@ -205,8 +399,8 @@ const styles = StyleSheet.create({
 		borderRadius: 28,
 		bottom: 16,
 		right: 16,
-		width: 52,
-		height: 52,
+		width: 48,
+		height: 48,
 		backgroundColor: colors.efesblue,
 		alignItems: 'center',
 		justifyContent: 'center',
@@ -217,14 +411,17 @@ const styles = StyleSheet.create({
 	tinyArrowLogo: {
 		width: 28,
 		height: 28,
+		zIndex: 1,
 	},
 	pulse: {
 		position: 'absolute',
-		width: 52,
-		height: 52,
+		width: 48,
+		height: 48,
 		borderRadius: 26,
 		backgroundColor: colors.efesblue,
 		opacity: 1,
+		zIndex: 1,
 	},
 });
+
 export default SellerDistributorStocks;
